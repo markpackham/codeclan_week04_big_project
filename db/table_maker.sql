94114693836e367f3c99d2c36cf4c12b62f79565
mark <markpackham1@gmail.com>
2020-04-23 13:59:06 +0100

    Note on table logic and order of deletion

--- db/table_maker.sql
+++ db/table_maker.sql
@@ -1,5 +1,9 @@
+DROP TABLE btable;
 DROP TABLE atable;
 
-CREATE TABLE atable (
-  id SERIAL primary key
+CREATE TABLE atable (id SERIAL primary key);
+
+CREATE TABLE btable (
+  id SERIAL PRIMARY KEY,
+  fk_id INT REFERENCES atable(id) ON DELETE CASCADE
 );
